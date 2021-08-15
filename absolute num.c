bec56ae8866c20ff726f52dc94db02b3666cdced
R.Vishalini devi <74974603+vishalinidevi@users.noreply.github.com>
2021-08-15 16:52:19 +0530

    Add files via upload

---
+++ absolute num.c
@@ -0,0 +1,36 @@
+/*You will be given a number 'num'. Your task is to print the absolute value of 'num'
+Example
+
+input: 
+ 6
+
+output: 
+ 6
+
+input: 
+ -3
+
+output: 
+ 3
+ */
+
+#include<stdio.h>
+
+int main()
+{
+    int num;
+    scanf("%d", &num);
+
+    //Write your code here
+    if(num>0)
+    {
+        printf("%d",num);
+    }
+    else
+    {
+        printf("%d",abs(num));
+    }
+    
+
+    return 0;
+}
